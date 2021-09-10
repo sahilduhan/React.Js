36858151749f42a2ba5470b13abede530e33d8c1
Sahil Duhan <duhan.sahil16@gmail.com>
2021-09-11 00:56:30 +0530

    babel

---
+++ learn_react/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function Greetings() {
+  <h1>hello world</h1>;
+}
+
+ReactDom.render(<Greetings></Greetings>, document.getElementById("root"));
